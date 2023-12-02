1a699cac4cc8162001ecaa6e9d368f7cd9e38111
Богдан Топорин <btoporin@mail.ru>
2023-12-02 22:48:28 +0300

    Initial Commit

---
+++ проект2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  проект2
+//
+//  Created by Богдан Топорин on 02.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ проект2/______2App.swift
@@ -0,0 +1,17 @@
+//
+//  ______2App.swift
+//  проект2
+//
+//  Created by Богдан Топорин on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ______2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
